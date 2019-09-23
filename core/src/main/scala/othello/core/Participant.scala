ffdc9b177c95a4c2f5cf2e137025f21cf96f1a9e
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-09-23 09:14:03 +0900

    ゲーム一覧表示改善中

--- core/src/main/scala/othello/core/Participant.scala
+++ core/src/main/scala/othello/core/Participant.scala
@@ -1,9 +1,12 @@
 package othello.core
 
-sealed trait Participant
+sealed trait Participant {
+  val name: ParticipantName
+}
 final case class Free() extends Participant
 final case class Wait() extends Participant
 final case class Watching() extends Participant
 final case class Gaming() extends Participant
 
 final case class ParticipantId(value: Int) extends AnyVal
+final case class ParticipantName(value: String) extends AnyVal
