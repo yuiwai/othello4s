4015b9a971c23cae4c0e07ef18b60b88f1247a8e
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2019-09-29 20:58:19 +0900

    OnlineServiceをリファクタリング

--- client/react/src/main/scala/othello/OnlineService.scala
+++ client/react/src/main/scala/othello/OnlineService.scala
@@ -12,7 +12,11 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class OnlineService extends Service[Future] with codec.Codec {
+  type WithServiceError[T] = Either[ServiceError, T]
   private val headers = Map("content-type" -> "application/json")
+  def get[R: io.circe.Decoder](url: String): Future[Either[io.circe.Error, R]] =
+    Ajax.get(url)
+      .map[Either[io.circe.Error, R]](r => decode[R](r.responseText))
   def post[R: io.circe.Decoder](url: String, json: Json = Json.Null): Future[Either[io.circe.Error, R]] =
     Ajax.post(url, json.noSpaces, headers = headers)
       .map[Either[io.circe.Error, R]](r => decode[R](r.responseText))
@@ -20,56 +24,26 @@ class OnlineService extends Service[Future] with codec.Codec {
     post[ParticipantId]("/participants/create", ParticipateRequest(name).asJson)
       .map(_.getOrElse(ParticipantId(0)))
   override def allGames(participantId: ParticipantId): Future[Seq[GameSummary]] =
-    Ajax
-      .post("/games", participantId.asJson.noSpaces, headers = headers)
-      .map(r => decode[Seq[GameSummary]](r.responseText).getOrElse(Seq.empty))
+    post[Seq[GameSummary]]("/games", participantId.asJson)
+      .map(_.getOrElse(Seq.empty))
   override def game(gameId: GameId): Future[Option[Game]] =
-    Ajax.get(s"/games/${gameId.value}")
-      .map(r => decode[Option[Game]](r.responseText)
-        .fold(_ => None, identity)
-      )
+    get[Option[Game]](s"/games/${gameId.value}")
+      .map(_.fold(_ => None, identity))
   override def createGame(participantId: ParticipantId): Future[Either[ServiceError, GameSummary]] =
-    Ajax
-      .post("/games/create", participantId.asJson.noSpaces, headers = headers)
-      .map(r =>
-        decode[Either[ServiceError, GameSummary]](r.responseText)
-          .fold[Either[ServiceError, GameSummary]](_ => Left(DecodeError), identity)
-      )
+    post[WithServiceError[GameSummary]]("/games/create", participantId.asJson)
+      .map(_.fold[Either[ServiceError, GameSummary]](_ => Left(DecodeError), identity))
   def entry(gameId: GameId, participantId: ParticipantId): Future[Either[ServiceError, EntryId]] =
-    Ajax
-      .post("/games/entry", EntryRequest(gameId, participantId).asJson.noSpaces, headers = headers)
-      .map(r =>
-        decode[Either[ServiceError, EntryId]](r.responseText)
-          .fold(_ => {
-            Left(DecodeError)
-          }, identity)
-      )
+    post[WithServiceError[EntryId]]("/games/entry", EntryRequest(gameId, participantId).asJson)
+      .map(_.fold(_ => Left(DecodeError), identity))
   def putStone(gameId: GameId, participantId: ParticipantId, pos: Pos): Future[Either[ServiceError, Game]] =
-    Ajax
-      .post("/games/putStone", PutStoneRequest(gameId, participantId, pos).asJson.noSpaces, headers = headers)
-      .map { r =>
-        decode[Either[ServiceError, Game]](r.responseText)
-          .fold(_ => {
-            Left(DecodeError)
-          }, identity)
-      }
+    post[WithServiceError[Game]]("/games/putStone", PutStoneRequest(gameId, participantId, pos).asJson)
+      .map(_.fold(_ => Left(DecodeError), identity))
   override def pass(gameId: GameId, participantId: ParticipantId): Future[Either[ServiceError, Game]] =
-    Ajax
-      .post("/games/pass", PassRequest(gameId, participantId).asJson.noSpaces, headers = headers)
-      .map { r =>
-        decode[Either[ServiceError, Game]](r.responseText)
-          .fold(_ => {
-            Left(DecodeError)
-          }, identity)
-      }
-
+    post[WithServiceError[Game]]("/games/pass", PassRequest(gameId, participantId).asJson)
+      .map(_.fold(_ => Left(DecodeError), identity))
   override def giveUp(gameId: GameId, participantId: ParticipantId): Future[Either[ServiceError, Game]] =
-    Ajax
-      .post("/games/giveUp", GiveUpRequest(gameId, participantId).asJson.noSpaces, headers = headers)
-      .map { r =>
-        decode[Either[ServiceError, Game]](r.responseText)
-          .fold(_ => Left(DecodeError), identity)
-      }
+    post[WithServiceError[Game]]("/games/giveUp", GiveUpRequest(gameId, participantId).asJson)
+      .map(_.fold(_ => Left(DecodeError), identity))
 
   // DEBUG
   override def createCustomGame(participantId: ParticipantId, board: Board): Future[Option[GameId]] =
